c61e498c7820f7d26250d0c782162240a6039784
imnaeem <ceinfo.pk@gmail.com>
2021-11-12 03:12:53 +0500

    Answers Functions generated

--- app/src/main/java/com/example/task3/Quiz.java
+++ app/src/main/java/com/example/task3/Quiz.java
@@ -2,7 +2,10 @@ package com.example.task3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -18,14 +21,77 @@ public class Quiz extends AppCompatActivity {
     String[] lisaveyah = {"ظ","ذ","ث","ص","س","ز"};
     String[] ghunna = {"م","ن", "ف","ب","م","و","باَ","بوُ","بىِ"};
 
+    Button halqiyah_btn, lahatiyah_btn, shajariyah_haafiyah_btn, tarfiyah_btn, nit_eeyah_btn, lisaveyah_btn, ghunna_btn;
+
     int len = halqiyah.length;
 
     final int cate = new Random().nextInt(7) + 0;
+    boolean user_answer = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        halqiyah_btn =  findViewById(R.id.halqiyahbtn);
+        lahatiyah_btn = findViewById(R.id.lahatiyahbtn);
+        shajariyah_haafiyah_btn = findViewById(R.id.shajariyahbtn);
+        tarfiyah_btn= findViewById(R.id.tarfiyahbtn);
+        nit_eeyah_btn=findViewById(R.id.niteeyahbtn);
+        lisaveyah_btn=findViewById(R.id.lisaveyahbtn);
+        ghunna_btn = findViewById(R.id.ghunnabtn);
+
+        halqiyah_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        lahatiyah_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        shajariyah_haafiyah_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        tarfiyah_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        nit_eeyah_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        lisaveyah_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ghunna_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
 
         word = findViewById(R.id.viewtext);
 
@@ -34,7 +100,7 @@ public class Quiz extends AppCompatActivity {
 
     }
 
-    public void getrandomword(int ran_cate)
+    public void getRandomword(int ran_cate)
     {
         final int ran_word;
         if (ran_cate==0)
@@ -61,9 +127,42 @@ public class Quiz extends AppCompatActivity {
         }
     }
 
-    public void getanswer()
+    public void getAnswer(int myans, String userans)
     {
+        if(myans==0 && userans=="halqiyahbtn")
+        {
+            user_answer = true;
+        }else if (myans==1 && userans=="lahatiyahbtn")
+        {
+            user_answer = true;
+        }else if (myans==2 && userans=="shajariyahbtn")
+        {
+            user_answer = true;
+        }else if (myans==3 && userans=="tarfiyahbtn")
+        {
+            user_answer = true;
+        }else if (myans==4 && userans=="niteeyahbtn")
+        {
+            user_answer = true;
+        }else if (myans==5 && userans=="lisaveyahbtn")
+        {
+            user_answer = true;
+        }else if (myans==6 && userans=="ghunna")
+        {
+            user_answer = true;
+        }
+        else
+        {
+            user_answer = false;
+        }
+    }
 
+    public void displayAnswer()
+    {
+        if (user_answer)
+        {
+
+        }
     }
 
 
