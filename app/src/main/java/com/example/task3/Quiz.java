0be4df21fc44cf865141475a03f574b09db2f83b
imnaeem <ceinfo.pk@gmail.com>
2021-11-12 02:55:03 +0500

    Generate random word function

--- app/src/main/java/com/example/task3/Quiz.java
+++ app/src/main/java/com/example/task3/Quiz.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class Quiz extends AppCompatActivity {
 
     TextView word;
@@ -16,6 +18,9 @@ public class Quiz extends AppCompatActivity {
     String[] lisaveyah = {"ظ","ذ","ث","ص","س","ز"};
     String[] ghunna = {"م","ن", "ف","ب","م","و","باَ","بوُ","بىِ"};
 
+    int len = halqiyah.length;
+
+    final int cate = new Random().nextInt(7) + 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,37 @@ public class Quiz extends AppCompatActivity {
 
     }
 
+    public void getrandomword(int ran_cate)
+    {
+        final int ran_word;
+        if (ran_cate==0)
+        {
+            ran_word= new Random().nextInt(halqiyah.length) + 0;
+        }else if(ran_cate==1)
+        {
+            ran_word= new Random().nextInt(lahatiyah.length) + 0;
+        }else if(ran_cate==2)
+        {
+            ran_word= new Random().nextInt(shajariyah_haafiyah.length) + 0;
+        }else if(ran_cate==3)
+        {
+            ran_word= new Random().nextInt(tarfiyah.length) + 0;
+        }else if(ran_cate==4)
+        {
+            ran_word= new Random().nextInt(nit_eeyah.length) + 0;
+        }else if(ran_cate==5)
+        {
+            ran_word= new Random().nextInt(lisaveyah.length) + 0;
+        }else if(ran_cate==6)
+        {
+            ran_word= new Random().nextInt(ghunna.length) + 0;
+        }
+    }
+
+    public void getanswer()
+    {
+
+    }
 
 
 }
