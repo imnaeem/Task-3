8edf78e35ee0942fe383c3d80f7823441ffe0c00
imnaeem <ceinfo.pk@gmail.com>
2021-11-11 14:32:24 +0500

    Repository Button working

--- app/src/main/java/com/example/task3/MainActivity.java
+++ app/src/main/java/com/example/task3/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.task3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button repo_btn;
+    String url ="https://github.com/imnaeem/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        repo_btn = findViewById(R.id.repo);
+
+        repo_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebPage(url);
+            }
+        });
     }
+    
+    
+    
+    public void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new
+                Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
 }
